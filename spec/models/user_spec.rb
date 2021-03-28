052de3076449af8302419f070992442fc87199be
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-03-28 16:35:37 +0900

    userモデルに対してのテストの実装を開始

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前、メール、パスワード、性別があれば有効な状態であること" do
+    user = User.new(
+      name: "太郎",
+      email: "tarou@gmail.com",
+      password: "foobar",
+      sex: 1,
+    )
+
+    expect(user).to be_valid
+  end
 end
