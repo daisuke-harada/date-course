4168043130754cf323865b08a5856e9e722c9e3f
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-03 21:40:45 +0900

    ユーザーの統合テストをリファクタリングした

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,12 +5,27 @@ RSpec.feature "Users", type: :feature do
     user = FactoryBot.build(:user)
     visit root_path
     click_link "新規登録"
-    fill_in "名前", with: user.name
-    fill_in "メール", with: user.email
-    choose "男性"
-    fill_in "パスワード", with: user.password
-    fill_in "パスワード(確認)", with: user.password_confirmation
+    input_registration user
+    expect {
+      click_button "登録"
+      user_show_display user
+    }.to change(User.all, :count).by(1)
+  end
 
+  scenario "新規登録画面からログイン画面に遷移し、ログインする" do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link "新規登録"
+    click_link "ログインはこちら"
+    input_login_information user
+  end
+
+  scenario "ログイン画面から新規登録画面に遷移し、新規登録する" do
+    user = FactoryBot.build(:user)
+    visit root_path
+    click_link "ログイン"
+    click_link "新規登録はこちら"
+    input_registration user
     expect {
       click_button "登録"
       user_show_display user
@@ -29,4 +44,20 @@ RSpec.feature "Users", type: :feature do
     click_button "ゲストログイン(1クリック)"
     user_show_display user
   end
+end
+
+# ユーザーのshowページの情報を表示する
+def user_show_display(user)
+  aggregate_failures do
+    expect(page).to have_content "#{user.name}"
+    expect(page).to have_content "#{user.change_sex_data_string}"
+  end
+end
+
+def input_registration(user)
+  fill_in "名前", with: user.name
+  fill_in "メール", with: user.email
+  choose "男性"
+  fill_in "パスワード", with: user.password
+  fill_in "パスワード(確認)", with: user.password_confirmation
 end
--- spec/support/user_support.rb
+++ spec/support/user_support.rb
@@ -1,16 +1,12 @@
 module UserSupport
-  # ユーザーのshowページの情報を表示する
-  def user_show_display(user)
-    aggregate_failures do
-      expect(page).to have_content "#{user.name}"
-      expect(page).to have_content "#{user.change_sex_data_string}"
-    end
-  end
-
-  # ログインを行う
+   # ログインを行う
   def sign_in_as(user)
     visit root_path
     click_link "ログイン"
+    input_login_information user
+  end
+
+  def input_login_information(user)
     fill_in "名前", with: user.name
     fill_in "パスワード", with: user.password
     click_button "ログイン"
