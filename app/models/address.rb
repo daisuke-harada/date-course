15f47080094f41a223a96dd7848345dbd2792393
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-04 17:33:51 +0900

    active hashをprefectureに導入。addressモデルの作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefectue
+  belongs_to_active_hash :prefecture
 end
--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :address do
-    
+    association :prefecture
   end
 end
