5780da6e2f581726e3be17837505fc9c3e03322a
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-07 23:50:59 +0900

    地域モデルをアクティブハッシュで作成、都道府県表示のレイアウトが完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
   def top
+    @areas = Area.all
+    @main_prefectures = Prefecture.find([13, 27, 14, 40, 23, 26, ])
   end
 end
---
+++ spec/features/addresses_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.feature "Addresses", type: :feature do
+  scenario "トップページから都道府県を選択し、その都道府県カテゴリのデートスポット一覧ページへ飛ぶ" do
+  end
+end
