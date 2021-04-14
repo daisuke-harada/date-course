0fb288aad4a53609304de16869cb9627ce06c389
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-14 22:14:10 +0900

    sessionコントローラをadminでログインできるように編集した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      flash[:success] = "ログインに成功しました。"
-      redirect_to user
+      administrator_confirm_log_in user
     else
       flash.now[:danger] = "ログインに失敗しました。"
       render 'new'
@@ -18,5 +15,16 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+end
+
+def administrator_confirm_log_in (user)
+  log_in user
+  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  flash[:success] = "ログインに成功しました。"
+  if user.admin == false
+    redirect_to user
+  elsif user.admin == true
+    redirect_to root_path
   end
 end
