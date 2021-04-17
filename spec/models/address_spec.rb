7beb16009ddd83f5d1a7a133e7ebeb00ac2d0303
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-17 23:38:17 +0900

    date_spotのモデルテストの改修

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,25 +8,5 @@ RSpec.describe Address, type: :model do
         expect(address).to be_valid
       end
     end
-
-    context "addressを保存できない場合" do
-      it "都道府県が選択されてなければ保存できないこと" do
-        address.prefecture_id = nil
-        address.valid?
-        expect(address.errors[:prefecture_id]).to include("can't be blank")
-      end
-
-      it "市の名前が入力されてなければ保存できないこと" do
-        address.city_name = nil
-        address.valid?
-        expect(address.errors[:city_name]).to include("can't be blank")
-      end
-
-      it "date_spot_idがnilならば保存できないこと" do
-        address.date_spot_id = nil
-        address.valid?
-        expect(address.errors[:date_spot_id]).to include("can't be blank")
-      end
-    end
   end
 end
