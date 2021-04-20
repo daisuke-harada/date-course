54477b35f1fdc70c970606e69d58e077e9030520
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-20 19:55:59 +0900

    デートスポット編集ページの統合テスト作成開始

--- spec/factories/date_spots.rb
+++ spec/factories/date_spots.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :date_spot do
     id {1}
-    name { "MyString" }
+    name { "test1" }
     opening_time { "08:00" }
     closing_time { "23:00" }
   end
--- spec/features/date_spot_spec.rb
+++ spec/features/date_spot_spec.rb
@@ -2,20 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "DateSpots", type: :feature do
   scenario "新規投稿画面で新規投稿に成功する" do
-    user = FactoryBot.create(:admin)
+    admin = FactoryBot.create(:admin)
     date_spot = FactoryBot.build(:date_spot) 
     address = date_spot.build_address(FactoryBot.attributes_for(:address))
-    sign_in_as user
+    sign_in_as admin
     click_link "デートスポット作成"
-    fill_in "デートスポット名", with: date_spot.name
-    find("#date_spot_address_attributes_prefecture_id").find("option[value='#{date_spot.address.prefecture_id}']").select_option
-    fill_in "市町村名、番地", with: date_spot.address.city_name 
-    "TODO: 営業時間"
+    date_spot_information_insert(date_spot)
     
-    fill_in "始業時間", with: date_spot.opening_time
-    fill_in "終業時間", with: date_spot.closing_time
-    
-    "TODO: カテゴリ登録"
     expect {
       click_button "登録"
       expect(page).to have_content "デートスポットの登録が完了しました"
@@ -24,10 +17,14 @@ RSpec.feature "DateSpots", type: :feature do
     }.to change(DateSpot.all, :count).by(1)
   end
 
-  scenario "デートスポット詳細ページからデートスポット編集ページに遷移し、デートスポット情報を編集する" do
-    user = FactoryBot.create(:admin)
-    date_spot = FactoryBot.create(:date_spot)
-    sign_in_as user
+  scenario "デートスポット詳細ページからデートスポット編集ページに遷移し、デートスポット情報を更新する" do
+    admin = FactoryBot.create(:admin)
+    date_spot = FactoryBot.create(:date_spot) 
+    date_spot.create_address(FactoryBot.attributes_for(:address))
+    date_spot.save
+    sign_in_as admin
+    click_link "デートスポットを探す"
+    click_button "このデートスポットを見る"
   end
 end
 
@@ -38,4 +35,13 @@ def date_spot_display(date_spot)
     expect(page).to have_content "#{date_spot.address.prefecture.name}"
     expect(page).to have_content "#{date_spot.address.city_name}"
   end
+end
+
+def date_spot_information_insert(date_spot)
+  fill_in "デートスポット名", with: date_spot.name
+  find("#date_spot_address_attributes_prefecture_id").find("option[value='#{date_spot.address.prefecture_id}']").select_option
+  fill_in "市町村名、番地", with: date_spot.address.city_name 
+  fill_in "始業時間", with: date_spot.opening_time
+  fill_in "終業時間", with: date_spot.closing_time
+  "TODO: カテゴリ登録"
 end
