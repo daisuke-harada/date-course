d70cb867ef25c733700ec5c791a6458fd72665a0
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-23 06:31:12 +0900

    fix

--- app/models/date_spot_review.rb
+++ app/models/date_spot_review.rb
@@ -1,4 +1,4 @@
 class DateSpotReview < ApplicationRecord
-  belongs_to user
-  belongs_to date_spot
+  belongs_to :user
+  belongs_to :date_spot
 end
--- db/migrate/20210422213058_create_date_spot_reviews.rb
+++ db/migrate/20210422213058_create_date_spot_reviews.rb
@@ -1,10 +1,10 @@
 class CreateDateSpotReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :date_spot_reviews do |t|
-      t.integer :user_id
-      t.integer :spot_id
       t.integer :rate
       t.text :content
+      t.references :user, null: false, foreign_key: true
+      t.references :date_spot, null: false, foreign_key: true
 
       t.timestamps
     end
