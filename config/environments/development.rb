8c22f512af174f7f68e147e03fcd68b675067902
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-23 21:08:40 +0900

    date_spotのseedデータを実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # 画像をseedデータで登録できるようにする
+  config.active_job.queue_adapter = :inline
 end
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,10 @@
-#環境別にseedファイルを読み込む
+# userデータ
 User.create(name: "guest", email: "guest@gmail.com", sex: 1, password: "foobar", password_confirmation: "foobar")
 User.create(name: "daisuke", email: "daisuke@gmail.com", sex: 1, password: "foobar", password_confirmation: "foobar")
 User.create(name: "admin", email: "adminstrator@gmail.com", sex: 1, password: "adminlogin1099", password_confirmation: "adminlogin1099", admin: true)
+
+# date_spotデータとaddressデータ
+DateSpot.create(name: "キャナルシティ", category_id: nil, opening_time: "08:00", closing_time: "23:00")
+DateSpot.find_by(name: "キャナルシティ").image.attach(io: File.open('app/assets/images/date_spot_images/canal.jpeg'),
+                                                    filename: 'canal.jpeg')
+Address.create(prefecture_id: 40, date_spot_id: 1, city_name: "福岡県福岡市博多区住吉１丁目２")
