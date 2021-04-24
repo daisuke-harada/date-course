4a22392ce4aa7af7ec9fe8b838d7b5e88eda0782
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-04-24 13:37:55 +0900

    デートコースレビュー機能実装開始

--- app/controllers/date_spot_reviews_controller.rb
+++ app/controllers/date_spot_reviews_controller.rb
@@ -1,9 +1,31 @@
 class DateSpotReviewsController < ApplicationController
+  before_action :logged_in_general_user, only: [:new, :edit, :update, :destroy]
+  before_action :correct_user,           only: [:edit, :update, :destroy]
+
   def new
-    date_spot = DateSpot.find(param[:id)
-    @date_spot_review = DateSpotRevew.new(date_spot_id: date_spot.id)
+    @date_spot_review = DateSpotReview.new(date_spot_id: params[:id])
   end
 
   def edit
   end
+
+  private
+
+    def logged_in_general_user
+      if logged_in? && current_user.admin == true
+        store_location
+        flash[:danger] = "一般ステータスのアカウントでログインしてください"
+        redirect_to login_url
+      elsif !logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # そのアカウントを持っている人以外がアクセスすることを防ぐ
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user 
+    end
 end
--- spec/features/date_spot_review_spec.rb
+++ spec/features/date_spot_review_spec.rb
@@ -4,15 +4,38 @@ RSpec.feature "DateSpotReviews", type: :feature do
   scenario "デートスポット詳細ページから口コミ新規投稿ページへ遷移し、口コミを新規投稿する" do
     user = FactoryBot.create(:user)
     date_spot = FactoryBot.create(:date_spot)
+    date_spot.create_address(FactoryBot.attributes_for(:address))
     date_spot_review = FactoryBot.build(:date_spot_review)
     sign_in_as user
     click_link "デートスポットを探す"
     click_link "このデートスポットを見る"
-    click_link "口コミを投稿する"
+    click_link "レビューする"
     fill_in "コメント", with: date_spot_review.content
     expect {
       click_button "投稿する"
       expect(page).to have_content "口コミを投稿しました"
     }.to change(DateSpotReview.all, :count).by(1)
   end
+
+  scenario "ログインせずにレビュー新規投稿ページへ遷移しようとすると、ログインページにリダイレクトされる" do
+    date_spot = FactoryBot.create(:date_spot)
+    date_spot.create_address(FactoryBot.attributes_for(:address))
+    visit root_path
+    click_link "デートスポットを探す"
+    click_link "このデートスポットを見る"
+    click_link "レビューする"
+    expect(page).to have_content "ログインしてください"
+  end
+
+  scenario "管理者でレビュー新規投稿ページへ遷移しようとすると、ログインページにリダイレクトされる" do
+    date_spot = FactoryBot.create(:date_spot)
+    date_spot.create_address(FactoryBot.attributes_for(:address))
+    admin = FactoryBot.create(:admin)
+    sign_in_as admin
+    click_link "デートスポットを探す"
+    click_link "このデートスポットを見る"
+    click_link "レビューする"
+    expect(page).to have_content "一般ステータスのアカウントでログインしてください"
+  end
+
 end
