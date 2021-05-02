abad0f123291f64026178152961fc1eedc72293b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-02 22:40:42 +0900

    画像アップロードのバリデーションを改修

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,7 +1,7 @@
 module ValidationHelper
   # 画像の形式のバリデーション
   def image_type
-    if !image.blob.content_type.in?(%('image/jpeg image/png'))
+    if image.attached? && !image.blob.content_type.in?(%('image/jpeg image/png'))
       errors.add(:image, 'はjpegまたはpng形式でアップロードしてください')
     end
   end
