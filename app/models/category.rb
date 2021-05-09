b3d7762606acf0985dccb01e16b4e0585f08b552
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-09 20:40:08 +0900

    fix

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ActiveHash::Base
+  self.date = [
+    {id:1, name: 'ショッピングモール'}, {id:2, name: '寿司'}, {id:3, name: '飲食店'},
+    {id:4, name: 'カフェ'},　{id:5, name: '居酒屋'},　{id:6, name: '焼肉'},　{id:7, name: 'バーベキュー'}
+  ]
+end
