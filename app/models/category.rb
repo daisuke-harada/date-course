0b1890a078d04990e56ba99371fd8542da60fac8
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-10 19:21:02 +0900

    カテゴリ機能実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveHash::Base
+  include ActiveHash::Associations
+
+  has_many :date_spot
+  
   self.date = [
     {id:1, name: 'ショッピングモール'}, {id:2, name: '寿司'}, {id:3, name: '飲食店'},
     {id:4, name: 'カフェ'},　{id:5, name: '居酒屋'},　{id:6, name: '焼肉'},　{id:7, name: 'バーベキュー'}
