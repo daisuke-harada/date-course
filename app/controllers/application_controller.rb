53e83248842595c4ea804ebbbc328657f5b23fea
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-13 20:02:49 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include UsersHelper
   
+  #デートスポットの名前検索の際に使用する
   def set_q_for_date_spot
     @date_spot_search_params = DateSpot.ransack(params[:q])
   end
 
+  #ユーザーの名前検索の際に使用する
   def set_q_for_user
     @user_search_params = User.ransack(params[:q])
   end
