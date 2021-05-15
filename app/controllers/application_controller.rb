fcf3578560b2cb8cc710d2c0f927f4f77697c923
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-15 12:29:13 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   #ユーザーの名前検索の際に使用する
   def set_q_for_user
     # 同時に１画面で同じパラメータを検索するため、違うパラメータを用意する。
-    @user_search_params = User.ransack(params[:p], search_key: :p)
+    @user_search_params = User.ransack(params[:q])
   end
 end
