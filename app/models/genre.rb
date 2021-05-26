4110f5fdc6719db24ead30c22ef5ac83317e82df
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-26 20:53:57 +0900

    デートスポット機能のソート機能実装開始

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,6 +5,7 @@ class Genre < ActiveHash::Base
 
   self.data = [
     {id: 1, name: 'ショッピングモール'}, {id: 2, name: '寿司'}, {id: 3, name: '飲食店'},
-    {id: 4, name: 'カフェ'}, {id: 5, name: '居酒屋'}, {id: 6, name: '焼肉'}, {id: 7, name: 'バーベキュー'}
+    {id: 4, name: 'カフェ'}, {id: 5, name: '居酒屋'}, {id: 6, name: '焼肉'}, {id: 7, name: 'バーベキュー'},
+    {id: 8, name: '遊園地'}, {id: 9, name: 'ランドマーク'}
   ]
 end
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -14,3 +14,18 @@ DateSpot.create(id: 2, name: "つなぐダイニング ZINO 天神店", genre_id
 DateSpot.find_by(id: 2).image.attach(io: File.open('app/assets/images/date_spot_images/tunagu_zino_hukuoka.jpeg'),
                                                     filename: 'tunagu_zino_hukuoka.jpeg')
 Address.create(prefecture_id: 40, date_spot_id: 2, city_name: "福岡市中央区大名１-１１-２２-１")
+
+DateSpot.create(id: 3, name: "東京ディズニーランド", genre_id: 8, opening_time: "10:00", closing_time: "19:00")
+DateSpot.find_by(id: 3).image.attach(io: File.open('app/assets/images/date_spot_images/disny.jpeg'),
+                                                    filename: 'disney.jpeg')
+Address.create(prefecture_id: 12, date_spot_id: 3, city_name: "浦安市舞浜１−１")
+
+DateSpot.create(id: 4, name: "東京スカイツリー", genre_id: 9, opening_time: "10:00", closing_time: "21:00")
+DateSpot.find_by(id: 4).image.attach(io: File.open('app/assets/images/date_spot_images/sky_tree.jpeg'),
+                                                    filename: 'sky_tree.jpeg')
+Address.create(prefecture_id: 13, date_spot_id: 4, city_name: "墨田区押上1-1-2")
+
+DateSpot.create(id: 5, name: "恵比寿ガーデンプレイス", genre_id: 1, opening_time: "07:00", closing_time: "00:00")
+DateSpot.find_by(id: 5).image.attach(io: File.open('app/assets/images/date_spot_images/garden_place.jpeg'),
+                                                    filename: 'garden_place.jpeg')
+Address.create(prefecture_id: 13, date_spot_id: 5, city_name: "渋谷区恵比寿４丁目２０ ガーデンプレイス")
