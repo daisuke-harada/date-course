0e48f30fa45d3d2e105194a19394dc31022ecab5
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-05-28 18:01:57 +0900

    営業時間検索機能実装

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module DateCourse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'Tokyo'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
