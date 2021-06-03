50ca00a96c844cc5e69b8f10345d95436666b42e
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-06-03 19:41:36 +0900

    topページのジャンルのメインのリンクが作成完了

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,7 +5,7 @@ class Genre < ActiveHash::Base
 
   self.data = [
     {id: 1, name: 'ショッピングモール'}, {id: 2, name: '飲食店'}, {id: 3, name: 'カフェ'},
-    {id: 4, name: 'レジャー'}, {id: 5, name: '遊園地'}, {id: 6, name: '水族館'},
+    {id: 4, name: 'アウトドア'}, {id: 5, name: '遊園地'}, {id: 6, name: '水族館'},
     {id: 7, name: '寿司'}, {id: 8, name: '居酒屋'}, {id: 9, name: '焼肉'},
     {id: 10, name: 'バーベキュー'}, {id: 11, name: 'ランドマーク'}
   ]
