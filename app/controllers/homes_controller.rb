6f3d87fadbce6c1eff22cc076887d7c4a6479962
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-06-03 19:35:43 +0900

    Topページのgenreリンクの作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,5 +5,6 @@ class HomesController < ApplicationController
   def top
     @areas = Area.all
     @main_prefectures = Prefecture.find([13, 27, 40, 14, 23, 26])
+    @main_genres = Genre.find([1, 2, 3, 4, 5, 6])
   end
 end
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -10,17 +10,17 @@ DateSpot.find_by(id: 1).image.attach(io: File.open('app/assets/images/date_spot_
                                                     filename: 'canal.jpeg')
 Address.create(prefecture_id: 40, date_spot_id: 1, city_name: "福岡市博多区住吉１丁目２")
 
-DateSpot.create(id: 2, name: "つなぐダイニング ZINO 天神店", genre_id: 5, opening_time: "2000-01-01 20:00", closing_time: "2000-01-02 05:00")
+DateSpot.create(id: 2, name: "つなぐダイニング ZINO 天神店", genre_id: 8, opening_time: "2000-01-01 20:00", closing_time: "2000-01-02 05:00")
 DateSpot.find_by(id: 2).image.attach(io: File.open('app/assets/images/date_spot_images/tunagu_zino_hukuoka.jpeg'),
                                                     filename: 'tunagu_zino_hukuoka.jpeg')
 Address.create(prefecture_id: 40, date_spot_id: 2, city_name: "福岡市中央区大名１-１１-２２-１")
 
-DateSpot.create(id: 3, name: "東京ディズニーランド", genre_id: 8, opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 19:00")
+DateSpot.create(id: 3, name: "東京ディズニーランド", genre_id: 5, opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 19:00")
 DateSpot.find_by(id: 3).image.attach(io: File.open('app/assets/images/date_spot_images/disny.jpeg'),
                                                     filename: 'disney.jpeg')
 Address.create(prefecture_id: 12, date_spot_id: 3, city_name: "浦安市舞浜１−１")
 
-DateSpot.create(id: 4, name: "東京スカイツリー", genre_id: 9, opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 21:00")
+DateSpot.create(id: 4, name: "東京スカイツリー", genre_id: 11, opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 21:00")
 DateSpot.find_by(id: 4).image.attach(io: File.open('app/assets/images/date_spot_images/sky_tree.jpeg'),
                                                     filename: 'sky_tree.jpeg')
 Address.create(prefecture_id: 13, date_spot_id: 4, city_name: "墨田区押上1-1-2")
