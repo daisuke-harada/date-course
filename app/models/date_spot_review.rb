6237a812ff1505a262b7a7fa43727917061a829b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-08-14 12:58:04 +0900

    fix

--- app/models/date_spot_review.rb
+++ app/models/date_spot_review.rb
@@ -3,6 +3,7 @@ class DateSpotReview < ApplicationRecord
   belongs_to :date_spot
 
   validates :content, presence: true, length: { maximum: 300 }
+  
   # user_idとdate_spot_idの二つの組み合わせに対して一意制約のバリデーションをかける
   validates :user_id, presence: true, :uniqueness => {:scope => :date_spot_id}
   validates :date_spot_id, presence: true
