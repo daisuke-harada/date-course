c7fdb44cd13d5666fd48b00c511757edfdc5eda5
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-11-22 20:46:53 +0900

    本番環境用にpuma.rb nginxフォルダ Dockerfile.production docker-compose.productionを修正

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,4 @@ plugin :tmp_restart
 #デプロイ時に必要
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
