694599864f10c3f23285fc06c0300010cc1a0786
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-11-23 21:30:22 +0900

    config/webpack/environment.jsを編集

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
-const { environment } = require('@rails/webpacker')
+function hotfixPostcssLoaderConfig (subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+}
 
-module.exports = environment
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
