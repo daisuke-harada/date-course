a6e7ae74d22b0acd6e3db015facaa5050114c2c4
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-11-27 21:02:47 +0900

    config/webpack/environment.jsを編集

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+// SplitChunksPlugin 共通で使用するpackageのJavaScriptファイルを纏める機能
+environment.splitChunks()
+
 module.exports = environment
 
-// function hotfixPostcssLoaderConfig (subloader) {
-//   const subloaderName = subloader.loader
-//   if (subloaderName === 'postcss-loader') {
-//     if (subloader.options.postcssOptions) {
-//       console.log(
-//         '\x1b[31m%s\x1b[0m',
-//         'Remove postcssOptions workaround in config/webpack/environment.js'
-//       )
-//     } else {
-//       subloader.options.postcssOptions = subloader.options.config;
-//       delete subloader.options.config;
-//     }
-//   }
-// }
-
-// environment.loaders.keys().forEach(loaderName => {
-//   const loader = environment.loaders.get(loaderName);
-//   loader.use.forEach(hotfixPostcssLoaderConfig);
-// });
