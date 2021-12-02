ad45089fa603341e44e2591db52d6ed8f4faff29
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-02 17:19:28 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :purger
   config.assets.css_compressor = :sass
 
+  # JavaScriptの圧縮方式
+  config.assets.js_compressor = :uglifier
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
