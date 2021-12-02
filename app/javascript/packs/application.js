c54153000aec3ec6181cc2a6424c368e8e21a698
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 01:38:39 +0900

    application.jsの内容を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import "../channels"
 import "../css/tailwindcss.css"
 
 Rails.start()
