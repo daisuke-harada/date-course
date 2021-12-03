66300b4d2642d15e244093951f9c426205f9d157
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 12:06:54 +0900

    application_controller.rbを編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,16 +7,4 @@ class HomesController < ApplicationController
     @main_prefectures = Prefecture.find([13, 27, 40, 14, 23, 26])
     @main_genres = Genre.find([1, 2, 3, 4, 5, 6])
   end
-
-  private 
-  #デートスポットの名前検索の際に使用する
-  def set_q_for_date_spot
-    @date_spot_search_params = DateSpot.ransack(params[:date_spot_search], search_key: :date_spot_search)
-  end
-
-  #ユーザーの名前検索の際に使用する
-  def set_q_for_user
-    # 同時に１画面で同じパラメータを検索するため、違うパラメータを用意する。
-    @user_search_params = User.ransack(params[:q])
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,16 +71,5 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(current_user) unless @user == current_user 
     end
-
-    #デートスポットの名前検索の際に使用する
-    def set_q_for_date_spot
-      @date_spot_search_params = DateSpot.ransack(params[:date_spot_search], search_key: :date_spot_search)
-    end
-
-    #ユーザーの名前検索の際に使用する
-    def set_q_for_user
-      # 同時に１画面で同じパラメータを検索するため、違うパラメータを用意する。
-      @user_search_params = User.ransack(params[:q])
-    end
 end
 
