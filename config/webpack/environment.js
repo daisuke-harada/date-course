23299e1c1025c5179beebb48f640cb598a6ca388
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 10:43:30 +0900

    fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,4 @@
 const { environment } = require('@rails/webpacker')
 
-// SplitChunksPlugin 共通で使用するpackageのJavaScriptファイルを纏める機能
-environment.splitChunks()
-
 module.exports = environment
 
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,8 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+// SplitChunksPlugin 共通で使用するpackageのJavaScriptファイルを纏める機能
+environment.splitChunks()
+// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
+// environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
 
 module.exports = environment.toWebpackConfig()
