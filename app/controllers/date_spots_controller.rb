28ac439b39f5df12750dc1682da7cc6e14ef003c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 11:34:21 +0900

    application_controller.rbを編集

--- app/controllers/date_spots_controller.rb
+++ app/controllers/date_spots_controller.rb
@@ -80,4 +80,15 @@ class DateSpotsController < ApplicationController
         redirect_to login_url
       end
     end
+
+    #デートスポットの名前検索の際に使用する
+    def set_q_for_date_spot
+      @date_spot_search_params = DateSpot.ransack(params[:date_spot_search], search_key: :date_spot_search)
+    end
+
+    #ユーザーの名前検索の際に使用する
+    def set_q_for_user
+      # 同時に１画面で同じパラメータを検索するため、違うパラメータを用意する。
+      @user_search_params = User.ransack(params[:q])
+    end
 end
