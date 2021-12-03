546f22af36e380c621842056aef1c19849fca66b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 10:51:06 +0900

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('postcss-loader'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
