36bc0dff36fd9c02b1905f51980ef2afbc47bb75
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-03 10:32:52 +0900

    production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,9 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
-
-const { environment } = require('@rails/webpacker');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
+
+module.exports = environment.toWebpackConfig()
