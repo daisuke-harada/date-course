a96d5532b2e4ce39550d6070e883c6a152da3667
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-04 15:27:44 +0900

    webpack-plugin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,8 +5,5 @@ const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin())
 // SplitChunksPlugin 共通で使用するpackageのJavaScriptファイルを纏める機能
 environment.splitChunks()
-// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-
-// environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
 
 module.exports = environment.toWebpackConfig()
