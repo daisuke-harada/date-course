6d9e4588077b554a9710193db2ab527675bdfdf4
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-09 22:33:16 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
-  # config.asset_host = "https://mydatecoursesassets.s3.ap-northeast-1.amazonaws.com"
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
