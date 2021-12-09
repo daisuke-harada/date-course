8cdd0b855a3f1e8dc6ca9bfef7c9343345929c7b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-09 21:33:54 +0900

    config/initializers/asset_sync.rbを編集

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,13 +1,5 @@
 if defined?(AssetSync)
   AssetSync.configure do |config|
-<<<<<<< HEAD
-    config.fog_provider = 'AWS'
-    config.fog_directory = ENV['FOG_DIRECTORY'] # S3バケット名
-    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'] # IAMユーザのアクセスキー
-    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] # IAMユーザのシークレットキー
-    config.aws_session_token = ENV['AWS_SESSION_TOKEN'] if ENV.key?('AWS_SESSION_TOKEN')
-    config.fog_region = 'ap-northeast-1' # S3バケットのリージョン
-=======
     # config.enabled = false if Rails.env.development? # ここを追記
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
@@ -84,6 +76,5 @@ if defined?(AssetSync)
         Dir[File.join(packs_dir, '/**/**')]
       end
     end
->>>>>>> s3_asset
   end
 end
