8a39df60f5c4f38655f0ff69c1f99e4591e46521
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-29 22:15:02 +0900

    management_courseのmodelを作成

--- db/migrate/20211228062312_create_management_courses.rb
+++ db/migrate/20211228062312_create_management_courses.rb
@@ -1,7 +1,8 @@
 class CreateManagementCourses < ActiveRecord::Migration[6.1]
   def change
     create_table :management_courses do |t|
-
+      t.integer :course_id
+      t.integer :spot_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2021_12_28_062312) do
   end
 
   create_table "management_courses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "spot_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
