e9649c26628abf9c8cb709bb75d07e3a5310540a
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-12-31 15:24:15 +0900

    model改修完了

--- app/models/management.rb
+++ app/models/management.rb
@@ -1,4 +1,5 @@
 class Management < ApplicationRecord
   belongs_to :user
   has_many :management_date_spots, dependent: :destroy
+  validates :user_id, presence: true
 end
--- app/models/management_date_spot.rb
+++ app/models/management_date_spot.rb
@@ -1,5 +1,7 @@
 class ManagementDateSpot < ApplicationRecord
   belongs_to :management
   belongs_to :date_spot
-  
+  validates :management_id, presence: true
+  validates :date_spot_id, presence: true
+  validates :procedure, presence: true
 end
