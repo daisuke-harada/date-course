42b6617189d387e351fb413db2c35d136edfb2c4
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-01-01 08:42:55 +0900

    デートコース作成機能のsession管理を整える。同じ名前のデートスポットを追加できないようにする

--- spec/features/course_spec.rb
+++ spec/features/course_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Courses", type: :feature do
+  scenario "デートコース一覧画に遷移する" do
+    visit root_path
+    click_link "デートコースを探す"
+    expect(page).to have_content "デートコース一覧画面"
+  end
 end
--- spec/features/management_date_spot_spec.rb
+++ spec/features/management_date_spot_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "ManagementDateSpot", type: :feature do
+  scenario "デートコース作成画面に遷移する" do
+    user = FactoryBot.create(:user)
+    sign_in_as user
+    click_link "デートコース作成"
+    expect(page).to have_content "デートコース作成画面"
+  end
 end
