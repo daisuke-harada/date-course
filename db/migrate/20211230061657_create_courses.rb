87159707fcad6aa5c19b26f12bd3afbd1f01dc4c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-01-20 16:26:33 +0900

    デートスポット登録確認画面を作成。デートスポット登録機能を実装

--- db/migrate/20211230061657_create_courses.rb
+++ db/migrate/20211230061657_create_courses.rb
@@ -2,7 +2,8 @@ class CreateCourses < ActiveRecord::Migration[6.1]
   def change
     create_table :courses do |t|
       t.references :user, null: false, foreign_key: true
-      t.boolean :authority, default: false
+      t.datetime :scheduled_time
+      t.boolean :authority, default: true
       t.timestamps
     end
   end
