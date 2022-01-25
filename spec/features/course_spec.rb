81f3cf52eef6f5e3ade7248a4ab211da479f9fb3
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-01-25 16:19:40 +0900

    JavaScriptのコードリファクタリング

--- spec/features/course_spec.rb
+++ spec/features/course_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "Courses", type: :feature do
   scenario "デートコース一覧画に遷移する" do
     visit root_path
     click_link "デートコースを探す"
-    expect(page).to have_content "デートコース一覧画面"
+    expect(page).to have_content "デートコース一覧ページ"
   end
 end
