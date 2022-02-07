57e1fa8d0ff4a4594358350b3f0cad7f8b1f1284
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-08 00:36:49 +0900

    fix

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -7,7 +7,7 @@ import axios from "axios"
 
 // ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
 const options = {
-  ignoreHeaders: true 
+  ignoreHeaders: true
 }
 
 export const client = applyCaseMiddleware(axios.create({
