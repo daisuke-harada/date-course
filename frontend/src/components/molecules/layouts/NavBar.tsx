aa51020c65b237296b4845b5c5dc1fa09b8e0c16
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-10 15:03:59 +0900

    navbarにルーティングを設定

--- frontend/src/components/molecules/layouts/NavBar.tsx
+++ frontend/src/components/molecules/layouts/NavBar.tsx
@@ -1,4 +1,7 @@
 import { memo, VFC } from "react";
+import { Link } from "react-router-dom";
+import { headerBottomRoutes } from "../../../router/HeaderBottomRoutes";
+import { headerTopLeftRoutes } from "../../../router/HeaderTopLeftRoutes";
 
 type Props = {
   isOpen: boolean
@@ -8,7 +11,8 @@ export const NavBar: VFC<Props> = memo((props) => {
   return(
   <div className={`${isOpen? `fixed`: `hidden`} lg:hidden sm:top-24 top-20 text-center border border-red-200 bg-red-200 z-50 w-2/3 right-0`}>
     <ul>
-      <li>ありがとう</li>
+      {headerBottomRoutes.map((route) => <li className=" border-b-2 p-2 my-3"><Link className="text-black" to={route.path}>{route.text}</Link></li>)}
+      {headerTopLeftRoutes.map((route) =><div className="p-2 my-3"><Link to={route.path} >{route.element}</Link></div>)}
     </ul>
   </div>
   );
