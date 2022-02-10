ca9f64748dd94ff24d05944e5154049eabbb2aa0
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-10 17:17:07 +0900

    イベントの型変換を試す

--- frontend/src/components/molecules/layouts/NavBar.tsx
+++ frontend/src/components/molecules/layouts/NavBar.tsx
@@ -13,7 +13,7 @@ export const NavBar: VFC<Props> = memo((props) => {
   return(
   <div className={`${isOpen? `fixed`: `hidden`} lg:hidden sm:top-24 top-20 text-center border border-red-200 bg-red-200 z-50 w-2/3 right-0`}>
     <ul>
-      {headerBottomRoutes.map((route) => <li className="border-b-2 p-2 my-3" onClick={onClickNavBarSwitch} key={route.path} ><Link className="text-black font-bold" to={route.path} >{route.text}</Link></li>)}
+      {headerBottomRoutes.map((route) => <li className="border-b-2 p-2 py-3 hover:bg-red-300" key={route.path} ><Link className="text-black font-bold" to={route.path} onClick={onClickNavBarSwitch} >{route.text}</Link></li>)}
       {headerTopLeftRoutes.map((route) =><li className="p-2 my-3" onClick={onClickNavBarSwitch} key={route.path} ><Link to={route.path} >{route.element}</Link></li>)}
       <li className="p-2 my-3" onClick={onClickNavBarSwitch} ><GuestLoginButton buttonSize={"w-full"}>ゲストログイン</GuestLoginButton></li>
     </ul>
