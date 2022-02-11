72badb29178e800f8019d82c511186d17bbf55b0
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-11 12:42:23 +0900

    ハンバーガーメニューを改修

--- frontend/src/components/molecules/layouts/NavBar.tsx
+++ frontend/src/components/molecules/layouts/NavBar.tsx
@@ -11,12 +11,10 @@ type Props = {
 export const NavBar: VFC<Props> = memo((props) => {
   const { isOpen, onClickNavBarSwitch } = props;
   return(
-  <div className={`${isOpen? `fixed`: `hidden`} lg:hidden sm:top-24 top-20 text-center border border-red-200 bg-red-200 z-50 w-2/3 right-0`}>
-    <ul>
+    <ul className={`${isOpen? `fixed`: `hidden`} lg:hidden sm:top-24 top-20 text-center border border-red-200 bg-red-200 z-50 w-2/3 right-0`}>
       {headerBottomRoutes.map((route) => <Link className="text-black font-bold" to={route.path} onClick={onClickNavBarSwitch} ><li className="border-b-2 p-2 py-3 hover:bg-red-300 hover:text-black" key={route.path} >{route.text}</li></Link>)}
       {headerTopLeftRoutes.map((route) => <li className="p-2 my-3" onClick={onClickNavBarSwitch} key={route.path} ><Link to={route.path} >{route.element}</Link></li>)}
       <li className="p-2 my-3" onClick={onClickNavBarSwitch} ><GuestLoginButton addClassNames={"w-full"}>ゲストログイン</GuestLoginButton></li>
     </ul>
-  </div>
   );
 });
