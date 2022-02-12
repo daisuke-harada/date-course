ed6539c20fb25150d1701e92373ba67e2b3c785f
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-12 17:07:57 +0900

    userモデルを作成し直す

--- backend/app/helpers/application_helper.rb
+++ backend/app/helpers/application_helper.rb
@@ -1,4 +1,3 @@
 module ApplicationHelper
   include HomesHelper
-  include SessionsHelper
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
       get 'homes/top'
       resources :users
       resources :date_spots
-      post   '/login',   to: 'sessions#create'
-      delete '/logout',  to: 'sessions#destroy'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
