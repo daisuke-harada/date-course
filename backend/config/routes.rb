ff193c6ea2f225ffa3bf2caba8ca933b8735d7ae
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-12 14:08:15 +0900

    backendのルーティングにログインとログアウトを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get 'homes/top'
       resources :users
       resources :date_spots
+      post   '/login',   to: 'sessions#create'
+      delete '/logout',  to: 'sessions#destroy'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
