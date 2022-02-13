51da9bbab3ad0aefd862a1b35e18e5cba5e1681c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-13 11:57:16 +0900

    新規登録ボタン作成

--- frontend/src/components/pages/homes/Top.tsx
+++ frontend/src/components/pages/homes/Top.tsx
@@ -1,6 +1,8 @@
 import { memo, VFC } from "react"
 export const Top: VFC = memo(() => {
   return(
-    <h1 className="text-indigo-900">トップページです</h1>
+    <>
+      <h1 className="text-indigo-900">トップページです</h1>
+    </>
   );
 });
