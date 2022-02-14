9a0ad77b9d6c3460b4ef722dcde8302e8814dcd1
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-14 12:43:05 +0900

    recoil使用開始

--- frontend/src/components/pages/homes/Top.tsx
+++ frontend/src/components/pages/homes/Top.tsx
@@ -1,14 +1,10 @@
 import { memo, VFC } from "react"
 
-type Props = {
-  loggedInStatus: boolean;
-}
 
-export const Top: VFC<Props> = memo((props) => {
-  const { loggedInStatus } = props;
+export const Top: VFC = memo(() => {
   return(
     <>
-      {loggedInStatus && (<h1>ログイン状態です</h1>)}
+      {/* {loggedInStatus && (<h1>ログイン状態です</h1>)} */}
       <h1 className="text-indigo-900">トップページです</h1>
     </>
   );
