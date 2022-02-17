22eb9e0c5febd4126971149b25feed3f5606f7c9
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-17 14:17:23 +0900

    user小第ページにデータを渡せるようにした

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    binding.pry
     render json: { user: @user }
   end
 end
--- frontend/src/components/pages/users/Show.tsx
+++ frontend/src/components/pages/users/Show.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, VFC } from "react";
+import { memo, useEffect, useState, VFC } from "react";
 import { useRecoilValue } from "recoil";
 import { loginStatusState } from "store/session";
 import { useParams } from "react-router-dom";
@@ -7,15 +7,22 @@ import { client } from "lib/api/client";
 export const Show: VFC = memo(() => {
   const getLoginStatus = useRecoilValue(loginStatusState);
   const {id} = useParams();
+  const [userName, setUserName] = useState<String>("");
+  const [userGender, setUserGender] = useState<String>("");
+
   useEffect(() => {
     console.log(id);
     client.get(`users/${id}`).then(response => {
-      console.log(response.data.user);
+      setUserName(response.data.user.name);
+      setUserGender(response.data.user.gender);
     });
-  });
+  }, [setUserName, setUserGender, id]);
+   console.log(userName);
   return(
     <>
       <h1>userのshowページです</h1>
+      <p>{userName}</p>
+      <p>{userGender}</p>
       {getLoginStatus.status && 'ログインしてるよー' }
     </>
   );
