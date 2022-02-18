ee124bde68e750d448c8fbae2c8e18ee77b0d61f
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-18 19:08:19 +0900

    FlashMessageの実装完了

--- frontend/src/components/atoms/message/FlashMessage.tsx
+++ frontend/src/components/atoms/message/FlashMessage.tsx
@@ -2,17 +2,17 @@ import { memo, useEffect, useState, VFC } from "react";
 import { useLocation } from "react-router-dom";
 import tw  from "tailwind-styled-components";
 
-const MessageDiv = tw.div`fixed right-0 rounded-2xl text-white text-md m-1 p-2`
+const MessageDiv = tw.div`fixed duration-1000 right-0 rounded-2xl text-white text-md m-1 p-2`
 
 export const FlashMessage: VFC = memo(() => {
-  const [condition, setCondition] = useState(true);
+  const [condition, setCondition] = useState('translate-x-0');
   const location = useLocation();
   const state = location.state as { message: string, type: string, condition: boolean};
 
-  // 変数の名前を変える
+  // メッセージのスライド
   const messageSwitch = ()=> {
-    state !== null && setTimeout(() => setCondition(false),2000);
-    state === null && (condition === false) && setCondition(true);
+    state !== null && setTimeout(() => setCondition('translate-x-96'),2000);
+    state === null && (condition === 'translate-x-96') && setCondition('translate-x-0');
   };
 
   useEffect(
@@ -22,7 +22,7 @@ export const FlashMessage: VFC = memo(() => {
 
   return (
     <>
-      {(state !== null && condition) && <MessageDiv className={ state.type }>{ state.message }</MessageDiv>}
+      {(state !== null && condition) && <MessageDiv className={ state.type + ' ' + condition }>{ state.message }</MessageDiv>}
     </>
   );
 });
