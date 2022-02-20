1ed10eab86a903bd7e11d92427c3dc75d998dc45
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-20 12:26:29 +0900

    active_hashを登録した。factories/userとmodel/userのrspecのテストを実装

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :gender, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, length: { maximum: 250 }
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
