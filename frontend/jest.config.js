dca49bde61fdf3d56da9dbc35fea330e52b62dd1
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-20 18:38:01 +0900

    jest.configを作成

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$'  : './node_modules/babel-jest',
+    '.*\\.(ts|tsx)$' : './node_modules/ts-jest',    // TypeScriptファイルをテストする場合
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx'] // テスト対象の拡張子を列挙する
+
 };
