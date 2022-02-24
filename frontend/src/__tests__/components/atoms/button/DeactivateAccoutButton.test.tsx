82aee3928172ed52ed3032f3b6db649e0f6cfe85
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-02-24 15:09:20 +0900

    @jest/globalsをインストールする

--- frontend/src/__tests__/components/atoms/button/DeactivateAccoutButton.test.tsx
+++ frontend/src/__tests__/components/atoms/button/DeactivateAccoutButton.test.tsx
@@ -1,26 +1,34 @@
 import { DeactivateAcountButton } from 'components/atoms/button/DeactivateAcountButton'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { currentUserState, loginStatusState } from 'store/session';
 Enzyme.configure({ adapter: new Adapter() });
 
+jest.mock('recoil');
+
 describe('DeactivateAcountButtonコンポーネントのテスト', () => {
-  it('ログインしているときはボタン名が表示される', () => {
+  it('ログインしていないときはボタンが表示されない', () => {
     const wrapper = shallow(
       <RecoilRoot>
         <DeactivateAcountButton />
       </RecoilRoot>
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.text()).toEqual('');
   });
 
-  it('ログインしていないときはボタン名が表示されない', () => {
+  it('ログインしているときはボタン名が表示される', () => {
+    const mockState = { state: true};
+    global.fetch = jest.fn(() => 
+      Promise.resolve({
+        json: () => Promise.resolve(mockState);
+      }))
     const wrapper = shallow(
       <RecoilRoot>
         <DeactivateAcountButton />
       </RecoilRoot>
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.text()).toEqual('退会');
   });
 
   it('ボタンを押すとdeleteアクションでapiに接続する', () => {
