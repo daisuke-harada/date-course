45e4932ae1b6faa5c420490f3f63846fca15e69c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-02 14:14:45 +0900

    fix

--- frontend/cypress/integration/sample.spec.ts
+++ frontend/cypress/integration/sample.spec.ts
@@ -2,15 +2,16 @@
 //とりあえず動いてるか
 describe('Users', () => {
   it('ログイン画面でログインを行う', () => {
-    //cy.server();
-    cy.visit('http://localhost:3000/login');
+    cy.visit('/login');
     cy.contains('ログイン画面');
     cy.contains('新規登録はこちら');
     cy.get('[data-e2e="name-input"]').type('daisuke');
     cy.get('[data-e2e="password-input"]').type('daisuke');
-    // cy.intercept('POST', 'http://localhost:7777/api/v1/login').as('loginUser');
+    cy.intercept('POST', 'api/v1/login',  (req) => {
+      req.reply({loginStatus: true, user: {name: 'daisuke', gender:'男'}});
+    });
     cy.get('[data-e2e="login-button"').click();
-    // cy.wait('@loginUser');
+    //cy.wait('@loginUser');
     cy.contains('daisuke');
     // cy.contains('ログインしてるよー');
   })
