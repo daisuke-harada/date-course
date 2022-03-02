a4e4deda3d26bb1e614fa8e623170b5c1bf05d50
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-02 11:53:52 +0900

    cypressのbaseUrlを削除

--- frontend/cypress/integration/sample.spec.ts
+++ frontend/cypress/integration/sample.spec.ts
@@ -3,15 +3,15 @@
 describe('Users', () => {
   it('ログイン画面でログインを行う', () => {
     //cy.server();
-    cy.visit('/login');
+    cy.visit('http://localhost:3000/login');
     cy.contains('ログイン画面');
     cy.contains('新規登録はこちら');
     cy.get('[data-e2e="name-input"]').type('daisuke');
     cy.get('[data-e2e="password-input"]').type('daisuke');
-    // cy.intercept('POST', '/api/v1/login').as('loginUser');
-    // cy.get('[data-e2e="login-button"').click();
-    // cy.await('@loginUser');
-    // cy.contains('daisuke');
+    // cy.intercept('POST', 'http://localhost:7777/api/v1/login').as('loginUser');
+    cy.get('[data-e2e="login-button"').click();
+    // cy.wait('@loginUser');
+    cy.contains('daisuke');
     // cy.contains('ログインしてるよー');
   })
 });
