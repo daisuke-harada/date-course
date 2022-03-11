7d349d133d259ef94a752b7a8b9fdc418ae9eeac
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-11 13:30:04 +0900

    backend: dateSpotの詳細ページと編集ページのrequestsテストコード、dateSpotのModelのテストコード 記述

--- backend/spec/factories/addresses.rb
+++ backend/spec/factories/addresses.rb
@@ -2,7 +2,15 @@ FactoryBot.define do
   factory :address do
     id { 1 }
     prefecture_id { Prefecture.find_by(id: 40).id }
-    city_name { "福岡市博多区住吉１丁目２" }
+    city_name { "福岡市博多区住吉1丁目2" }
     association :date_spot
   end
+
+  factory :other_address, class: Address do
+    id { 2 }
+    prefecture_id { Prefecture.find_by(id: 40).id }
+    city_name { "福岡市中央区大名1-11-22-1" }
+    association :date_spot, factory: :other_spot # oter_spotという名前のdate_spotモデルに対してアソシエーションを通す
+  end
+
 end
--- backend/spec/factories/date_spots.rb
+++ backend/spec/factories/date_spots.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     opening_time { "2000-01-01 08:00:00 UTC" }
     closing_time { "2000-01-01 23:00:00 UTC" }
   end
+
+  factory :other_spot, class: DateSpot do
+    id { 2 }
+    name { "つなぐダイニング ZINO 天神店" }
+    genre_id { Genre.find_by(id: 8).id }
+    opening_time { "2000-01-01 08:00:00 UTC" }
+    closing_time { "2000-01-01 23:00:00 UTC" }
+  end
 end
