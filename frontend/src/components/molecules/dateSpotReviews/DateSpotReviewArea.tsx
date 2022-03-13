236793c2e70a6b3517bfbcc8924a13bc018773e9
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-13 19:52:42 +0900

    fix

--- frontend/src/components/molecules/dateSpotReviews/DateSpotReviewArea.tsx
+++ frontend/src/components/molecules/dateSpotReviews/DateSpotReviewArea.tsx
@@ -1,12 +1,13 @@
+import { BaseButton } from "components/atoms/button/BaseButton";
+import { UserImage } from "components/atoms/layouts/UserImage";
 import { memo, useCallback, useEffect, useState, VFC } from "react";
 import { useRecoilValue } from "recoil";
 import { currentUserState } from "store/session";
 import tw from "tailwind-styled-components";
 
 const Div = tw.div`w-full flex`
-const ImageParentDiv = tw.div` h-40 w-40 relative`;
-const Image = tw.img`object-cover absolute top-0 w-full h-full rounded-2xl`;
-const Input = tw.input`my-5 border-b-2 outline-none w-1/2 mobile(M):w-full mobile(M):ml-0 ml-8`;
+const TextArea = tw.textarea`border-2 rounded-xl p-1 w-3/4 h-full`
+
 
 type Props = {
   dateSpotId: number
@@ -16,20 +17,27 @@ export const DateSpotReviewArea: VFC<Props> = memo((props) => {
   const { dateSpotId } = props;
   const getCurrentUser = useRecoilValue(currentUserState);
   const [content, setContent] = useState<string>('');
-  const [userImage, setUserImage] = useState<string>('http://localhost:7777/images/no_image.jpg');
-  const onChangeContent: React.ChangeEventHandler<HTMLInputElement> = useCallback((e) => setContent(e.target.value), []);
-  useEffect(() => {
-    getCurrentUser.user.image.url !== null && setUserImage(getCurrentUser.user.image.url);
-  }, [getCurrentUser]);
+  const onChangeContent: React.ChangeEventHandler<HTMLTextAreaElement> = useCallback((e) => setContent(e.target.value), []);
 
   return(
     <Div>
-      <ImageParentDiv>
-        <Image src={userImage} alt='UserProfileImage' />
-      </ImageParentDiv>
+      <UserImage user={getCurrentUser.user} addClassName='h-40 w-40' />
       {/* 星による評価 */}
-      <div className="m-5">
-        <Input placeholder='コメントを入力' value={content} onChange={onChangeContent} />
+      <div className="w-2/3 h-40 pt-5 px-5 flex flex-col">
+        <div>{getCurrentUser.user.name}</div>
+        <div className='flex my-2'>
+          <img src={`${process.env.PUBLIC_URL}/dateSpotReviewImages/star-on.png`} alt='star' />
+          <img src={`${process.env.PUBLIC_URL}/dateSpotReviewImages/star-on.png`} alt='star' />
+          <img src={`${process.env.PUBLIC_URL}/dateSpotReviewImages/star-on.png`} alt='star' />
+          <img src={`${process.env.PUBLIC_URL}/dateSpotReviewImages/star-on.png`} alt='star' />
+          <img src={`${process.env.PUBLIC_URL}/dateSpotReviewImages/star-on.png`} alt='star' />
+        </div>
+        <div className='flex w-full'>
+          <TextArea placeholder='コメントを入力' value={content} onChange={onChangeContent} />
+          <div className='text-center mt-4 w-1/4 pt-2 px-1'>
+            <BaseButton>投稿</BaseButton>
+          </div>
+        </div>
       </div>
     </Div>
   );
