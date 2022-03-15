578f99ba525de421b322a42f21239277d7144dfd
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-15 17:24:51 +0900

    DateSpotの詳細ページに評価の平均値を載せる

--- frontend/src/components/pages/dateSpots/Show.tsx
+++ frontend/src/components/pages/dateSpots/Show.tsx
@@ -11,6 +11,8 @@ import { Link } from "react-router-dom";
 import { AddressResponseData, DateSpotResponseData } from "types/dateSpots/response";
 import { Map } from "components/molecules/maps/Map";
 import { DateSpotReviewArea } from "components/organisms/dateSpotReviews/DateSpotReviewArea";
+import { StarRateText } from "components/atoms/layouts/StarRateText";
+//import { DateSpotReviewAndUserResponseData } from "types/dateSpotReviews/response";
 
 const MainDiv = tw.div`border border-black bg-white mt-10 m-20 p-5 rounded-2xl`;
 const DateSpotNameTitle = tw.h1`w-full m-5 text-sm font-bold md:text-3xl`;
@@ -26,6 +28,7 @@ export const Show: VFC = memo(() => {
   const [dateSpotReviews, setDateSpotReviews] = useState([]);
   const [genreName, setGenreName] = useState<string>('');
   const [dateSpotImage, setDateSpotImage] = useState('http://localhost:7777/images/no_image.jpg');
+  // const [dateSpotAverageRate, setDateSpotAverageRate] = useState(0);
 
   const getCurrentUser = useRecoilValue(currentUserState);
   const getLoginStatus = useRecoilValue(loginStatusState);
@@ -49,17 +52,9 @@ export const Show: VFC = memo(() => {
               <Image src={dateSpotImage} alt='DateSpotProfileImage' />
             </ImageParentDiv>
             <DateSpotNameTitle>{dateSpot?.name}</DateSpotNameTitle>
-            {/* <div className="w-full flex m-5" id = "date_spot_reviews_rate_average" >
-              <img src ="<%= asset_url('date_spot_review_images/star-off.png') %>" id ="star-average-1" className ="w-14 h-14">
-              <img src ="<%= asset_url('date_spot_review_images/star-off.png') %>" id ="star-average-2" className ="w-14 h-14">
-              <img src ="<%= asset_url('date_spot_review_images/star-off.png') %>" id ="star-average-3" className ="w-14 h-14">
-              <img src ="<%= asset_url('date_spot_review_images/star-off.png') %>" id ="star-average-4" className ="w-14 h-14">
-              <img src ="<%= asset_url('date_spot_review_images/star-off.png') %>" id ="star-average-5" className ="w-14 h-14">
-              <div className="mx-4 font-bold mt-3 md:text-3xl">評価</div>
-              <div className="font-bold mt-3 md:text-3xl" id = "rate_average_number">
-                <%= @date_spot_reviews_rate_average %>
-              </div>
-            </div> */}
+            <div className='ml-2'>
+              <StarRateText rate={0} size={60} />
+            </div>
             <BusinessHour openingTime={dateSpot?.openingTime} closingTime={dateSpot?.closingTime} />
             <div className="m-5 text-sm font-bold md:text-xl">
               {address?.cityName}
@@ -101,18 +96,6 @@ export const Show: VFC = memo(() => {
           <DateSpotReviewArea dateSpotId={dateSpot.id} dateSpotReviews={dateSpotReviews} setDateSpotReviews={setDateSpotReviews} />
         }
       </MainDiv>
-
-      {/* // <div className="border border-black mt-10 m-20">
-      //   <div className="m-5">
-      //     <%= link_to "レビューする", new_date_spot_date_spot_review_path(@date_spot), className: "btn btn-salmon" unless @current_user_date_spot_review %>
-      //   </div>
-
-      //   <!-- 非同期機能実装 -->
-      //   <%= render 'date_spot_reviews/date_spot_review_profile', {date_spot_review: @current_user_date_spot_review, target_image: @current_user_date_spot_review.user, target_name: @current_user_date_spot_review.user.name } if @current_user_date_spot_review %>
-      //   <% @reviews.each do |date_spot_review| %>
-      //     <%= render 'date_spot_reviews/date_spot_review_profile', {date_spot_review: date_spot_review, target_image: date_spot_review.user, target_name: date_spot_review.user.name } %>
-      //   <% end %>
-      // </div> */}
     </>
   );
 });
