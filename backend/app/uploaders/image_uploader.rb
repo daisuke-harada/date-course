9cc71b547c6b3d6c7c3937cfc8736ae04838fb91
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-23 16:20:03 +0900

    carrierwaveを編集

--- backend/app/uploaders/image_uploader.rb
+++ backend/app/uploaders/image_uploader.rb
@@ -5,13 +5,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  # if Rails.env.development?
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,14 +1,16 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.development?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = Rails.application.credentials[:FOG_DIRECTORY]
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials[:AWS_ACCESS_KEY_ID],
       aws_secret_access_key: Rails.application.credentials[:AWS_SECRET_ACCESS_KEY],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
-
-    config.fog_directory  = Rails.application.credentials[:FOG_DIRECTORY]
-    config.cache_storage = :fog
   else
     config.asset_host = "http://localhost:7777"
     config.storage = :file
