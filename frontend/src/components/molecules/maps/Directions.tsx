7bff0684d39b243d2eb60798a9f186dc475d9c77
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-29 16:42:40 +0900

    managementCoursesのglobalステートを変更する

---
+++ frontend/src/components/molecules/maps/Directions.tsx
@@ -0,0 +1,12 @@
+import { memo, useState, VFC } from "react";
+import { GoogleMap, LoadScript, InfoWindow, Marker} from "@react-google-maps/api";
+import { AddressAndDateSpotJoinData } from "types/dateSpots/response";
+
+
+export const Directions: VFC = memo((props) => {
+
+  return(
+    <>
+    </>
+  );
+});
--- frontend/src/components/pages/managementCourses/CreateCourse.tsx
+++ frontend/src/components/pages/managementCourses/CreateCourse.tsx
@@ -11,6 +11,7 @@ const MainDiv = tw.div`bg-white mt-10 m-20 py-5 px-10 shadow-xl rounded-2xl`;
 export const CreateCourse: VFC = memo(() => {
   const getCurrentUser = useRecoilValue(currentUserState);
   const [managementCourses, setManagementCourses] = useRecoilState(managementCourseState({userId: getCurrentUser.user.id}));
+  console.log(managementCourses.courseDuringSpotIdAndNames);
   return(
     <MainDiv>
       <h1 className="m-5 font-bold text-3xl pb-5">デートコース作成</h1>
@@ -21,19 +22,23 @@ export const CreateCourse: VFC = memo(() => {
             目的地は登録されていません。デートスポットをデートコースに追加してみましょう。
           </p>
           :
-          <div className="w-1/3">
-            {
-              managementCourses.courseDuringSpotIdAndNames.map((courseDuringSpotIdAndName ,index) => (
-                <CourseDuringSpotCard
-                  key={courseDuringSpotIdAndName.dateSpotId}
-                  addressAndDateSpotId={courseDuringSpotIdAndName.dateSpotId}
-                  managementCourses={managementCourses}
-                  setManagementCourses={setManagementCourses}
-                  courseNumber={index}
-                />
-              ))
-            }
-          </div>
+          <>
+            <div className="w-1/3">
+              {
+                managementCourses.courseDuringSpotIdAndNames.map((courseDuringSpotIdAndName ,index) => (
+                  <CourseDuringSpotCard
+                    key={courseDuringSpotIdAndName.dateSpotId}
+                    addressAndDateSpotId={courseDuringSpotIdAndName.dateSpotId}
+                    managementCourses={managementCourses}
+                    setManagementCourses={setManagementCourses}
+                    courseNumber={index}
+                  />
+                ))
+              }
+            </div>
+            <div className="w-2/3 bg-black mx-3 rounded-xl">
+            </div>
+          </>
         }
       </div>
       <div className="text-center">
