b430504b5a7545c1962d59e00a3e9c383129a68e
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-03-30 14:54:32 +0900

    ルート検索機能の実装完了

--- frontend/src/components/molecules/maps/Directions.tsx
+++ frontend/src/components/molecules/maps/Directions.tsx
@@ -1,12 +1,76 @@
-import { memo, useState, VFC } from "react";
-import { GoogleMap, LoadScript, InfoWindow, Marker} from "@react-google-maps/api";
+import { memo, useCallback, useEffect, useState, VFC } from "react";
+import { GoogleMap, DirectionsService, DirectionsRenderer } from "@react-google-maps/api";
+import { ManagementCourse } from "types/managementCourses/management";
 import { AddressAndDateSpotJoinData } from "types/dateSpots/response";
 
+type Props = {
+  managementCourses: ManagementCourse
+};
 
-export const Directions: VFC = memo((props) => {
+export const Directions: VFC<Props> = memo((props) => {
+  const { managementCourses } = props;
+
+  // directionsCallbackをデートスポットの情報を入れ替えた際にも変更できるように使用する
+  const [copyDuringSpots, setCopyDuringSpots] = useState<AddressAndDateSpotJoinData[]>([]);
+  const origin = { lat: managementCourses.courseDuringSpots[0].latitude, lng: managementCourses.courseDuringSpots[0].longitude};
+  const destination = { lat: managementCourses.courseDuringSpots[managementCourses.courseDuringSpots.length -1].latitude, lng: managementCourses.courseDuringSpots[managementCourses.courseDuringSpots.length -1].longitude};
+  const [transitPoints, setTransitPoints] = useState<google.maps.DirectionsWaypoint[] | undefined>(undefined);
+  const [currentDirection, setCurrentDirection] = useState<google.maps.DirectionsResult | null>(null);
+
+  const directionsCallback = useCallback((googleResponse) => {
+    if (googleResponse) {
+      if (currentDirection) {
+        if (
+          googleResponse === "OK" &&
+          currentDirection.geocoded_waypoints && googleResponse.geocoded_waypoints.length !== currentDirection.geocoded_waypoints.length
+        ) {
+          // ルートが変更されたのでstateを更新する
+          setCurrentDirection(googleResponse);
+        } else if(copyDuringSpots !== managementCourses.courseDuringSpots){
+          // デートスポットの順番が入れ替えられたためstateを更新する
+          setCurrentDirection(googleResponse);
+          setCopyDuringSpots(managementCourses.courseDuringSpots);
+        }
+      } else {
+        if (googleResponse.status === "OK") {
+          // 現在設定されているデートスポットのコピーを作成する。そうすることでデートスポットの順番が入れ替わった際にもステートを更新できるようにする。
+          setCopyDuringSpots(managementCourses.courseDuringSpots);
+          // 初めてルートが設定されたのでステートを更新する。
+          setCurrentDirection(googleResponse);
+        }
+      }
+    }
+  }, [currentDirection, managementCourses.courseDuringSpots, copyDuringSpots]);
+
+
+  useEffect(() => {
+    if(managementCourses.courseDuringSpots.length > 2){
+      const copyCourses = managementCourses.courseDuringSpots.slice();
+      copyCourses.splice( 0, 1);
+      copyCourses.splice(copyCourses.length - 1, 1);
+      // stopoverをtrueにすることで寄り道して行くことになる
+      setTransitPoints(copyCourses.map((course)=>({location: new google.maps.LatLng(course.latitude, course.longitude), stopover: true})));
+    }
+  }, [managementCourses.courseDuringSpots]);
 
   return(
-    <>
-    </>
+    <GoogleMap mapContainerClassName='w-full md:h-full h-96 rounded-2xl' zoom={17} >
+      <DirectionsService
+        options={{
+          origin: origin,
+          destination: destination,
+          travelMode: google.maps.TravelMode.DRIVING,
+          waypoints: transitPoints,
+        }}
+        callback={directionsCallback}
+      />
+      {currentDirection !== null && (
+        <DirectionsRenderer
+          options={{
+            directions: currentDirection,
+          }}
+        />
+      )}
+    </GoogleMap>
   );
 });
--- frontend/src/components/pages/managementCourses/CreateCourse.tsx
+++ frontend/src/components/pages/managementCourses/CreateCourse.tsx
@@ -5,45 +5,56 @@ import tw from "tailwind-styled-components";
 
 import { managementCourseState } from "store/managementCourse";
 import { currentUserState } from "store/session";
+import { Directions } from "components/molecules/maps/Directions";
+import { LoadScript } from "@react-google-maps/api";
+import { Map } from "components/molecules/maps/Map";
 
 const MainDiv = tw.div`bg-white mt-10 m-20 py-5 px-10 shadow-xl rounded-2xl`;
 
 export const CreateCourse: VFC = memo(() => {
   const getCurrentUser = useRecoilValue(currentUserState);
-  const [managementCourses, setManagementCourses] = useRecoilState(managementCourseState({userId: getCurrentUser.user.id}));
-  console.log(managementCourses.courseDuringSpots);
+  const [managementCourses, setManagementCourses] = useRecoilState(managementCourseState({userId: getCurrentUser.user.id}))
+
   return(
-    <MainDiv>
-      <h1 className="m-5 font-bold text-3xl pb-5">デートコース作成</h1>
-      <div className="w-full mt-5 flex">
-        {
-          managementCourses.courseDuringSpots.length === 0?
-          <p className="text-blue-600 text-center m-10 text-3xl">
-            目的地は登録されていません。デートスポットをデートコースに追加してみましょう。
-          </p>
-          :
-          <>
-            <div className="w-1/3">
-              {
-                managementCourses.courseDuringSpots.map((courseDuringSpot ,index) => (
-                  <CourseDuringSpotCard
-                    key={courseDuringSpot.dateSpot.id}
-                    courseDuringSpot={courseDuringSpot}
-                    managementCourses={managementCourses}
-                    setManagementCourses={setManagementCourses}
-                    courseNumber={index}
-                  />
-                ))
-              }
-            </div>
-            <div className="w-2/3 bg-black mx-3 rounded-xl">
-            </div>
-          </>
-        }
-      </div>
-      <div className="text-center">
-        {/* ボタンエリア */}
-      </div>
-    </MainDiv>
+      <MainDiv>
+        <h1 className="m-5 font-bold text-3xl pb-5">デートコース作成</h1>
+        <div className="w-full mt-5 flex">
+          {
+            managementCourses.courseDuringSpots.length === 0?
+            <p className="text-blue-600 text-center m-10 text-3xl">
+              目的地は登録されていません。デートスポットをデートコースに追加してみましょう。
+            </p>
+            :
+            <>
+              <div className="w-1/3">
+                {
+                  managementCourses.courseDuringSpots.map((courseDuringSpot ,index) => (
+                    <CourseDuringSpotCard
+                      key={courseDuringSpot.dateSpot.id}
+                      courseDuringSpot={courseDuringSpot}
+                      managementCourses={managementCourses}
+                      setManagementCourses={setManagementCourses}
+                      courseNumber={index}
+                    />
+                  ))
+                }
+              </div>
+              <div className="w-2/3 mx-3 rounded-xl">
+                {
+                  managementCourses.courseDuringSpots.length === 1?
+                  <Map  addressAndDateSpot={managementCourses.courseDuringSpots[0]} />
+                  :
+                  <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || ''} >
+                     <Directions managementCourses={managementCourses} />
+                  </LoadScript>
+                }
+              </div>
+            </>
+          }
+        </div>
+        <div className="text-center">
+          {/* ボタンエリア */}
+        </div>
+      </MainDiv>
   );
 });
