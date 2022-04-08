1c9726438d709020eef6da8673e94745dc0c5e2b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-04-08 15:46:51 +0900

    ジャンルに公演を追加

--- backend/db/seeds/development.rb
+++ backend/db/seeds/development.rb
@@ -1,23 +1,36 @@
-User.create(name: 'guest', email: 'guest@gmail.com', gender: '男性', image: File.open('./public/images/user_images/spiderman.png'), password: 'foobar', password_confirmation: 'foobar')
-User.create(name: 'daisuke', email: 'daisuke@gmail.com', gender: '男性', password: 'daisuke', password_confirmation: 'daisuke')
-User.create(name: 'peter', email: 'kenta@gmail.com', gender: '男性', image: File.open('./public/images/user_images/spiderman.png'), password: 'foobar', password_confirmation: 'foobar')
-User.create(name: 'marika', email: 'marika@gmail.com', gender: '女性', password: 'marika', password_confirmation: 'marika')
-User.create(name: 'admin', email: 'adminstrator@gmail.com', gender: '男性', password: 'adminadmin', password_confirmation: 'adminadmin', admin: true)
+def spot_image(id)
+  return "#{Genre.find(id).name}.jpg"
+end
 
-DateSpot.create(id: 1, name: 'キャナルシティ博多', genre_id: 1, image: File.open('./public/images/date_spot_images/canal.jpeg'), opening_time: '2000-01-01 08:00', closing_time: '2000-01-01 23:00')
-Address.create(prefecture_id: 40, date_spot_id: 1, city_name: Prefecture.find(40).name + '福岡市博多区住吉1丁目2')
+def normal_time(time)
+  return "2000-01-01 #{time}"
+end
 
-DateSpot.create(id: 2, name: 'つなぐダイニング ZINO 天神店', genre_id: 8, image: File.open('./public/images/date_spot_images/tunagu_zino_hukuoka.jpeg'), opening_time: '2000-01-01 20:00', closing_time: '2000-01-02 05:00')
-Address.create(prefecture_id: 40, date_spot_id: 2, city_name: Prefecture.find(40).name + '福岡市中央区大名1-11-22-1')
+# 深夜0時から5時までの時間帯のデータを入れる際に使用
+def midnight__time(time)
+  return "2000-01-02 #{time}"
+end
 
-DateSpot.create(id: 3, name: "東京ディズニーランド", genre_id: 5, image: File.open('./public/images/date_spot_images/disny.jpeg'), opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 19:00")
-Address.create(prefecture_id: 12, date_spot_id: 3, city_name: Prefecture.find(12).name + "浦安市舞浜1-1")
+def SpotAndAddressCreate(name, genre_id, opening_time, closing_time, prefecture_id, city_name)
+  date_spot = DateSpot.create(name: name, genre_id: genre_id, image: File.open("./public/images/date_spot_images/#{spot_image(genre_id)}"), opening_time: opening_time, closing_time: closing_time)
+  Address.create(prefecture_id: prefecture_id, date_spot_id: date_spot.id, city_name: Prefecture.find(prefecture_id).name + city_name)
+end
 
-DateSpot.create(id: 4, name: "東京スカイツリー", genre_id: 11, image: File.open('./public/images/date_spot_images/sky_tree.jpeg'), opening_time: "2000-01-01 10:00", closing_time: "2000-01-01 21:00")
-Address.create(prefecture_id: 13, date_spot_id: 4, city_name: Prefecture.find(13).name + "墨田区押上1-1-2")
+User.create(name: "guest", email: "guest@gmail.com", gender: "男性", image: File.open("./public/images/user_images/spiderman.png"), password: "foobar", password_confirmation: "foobar")
+User.create(name: "daisuke", email: "daisuke@gmail.com", gender: "男性", password: "daisuke", password_confirmation: "daisuke")
+User.create(name: "peter", email: "kenta@gmail.com", gender: "男性", image: File.open("./public/images/user_images/spiderman.png"), password: "foobar", password_confirmation: "foobar")
+User.create(name: "marika", email: "marika@gmail.com", gender: "女性", password: "marika", password_confirmation: "marika")
+User.create(name: "admin", email: "adminstrator@gmail.com", gender: "男性", password: "adminadmin", password_confirmation: "adminadmin", admin: true)
 
-DateSpot.create(id: 5, name: "恵比寿ガーデンプレイス", genre_id: 1, image: File.open('./public/images/date_spot_images/garden_place.jpeg'), opening_time: "2000-01-01 07:00", closing_time: "2000-01-02 00:00")
-Address.create(prefecture_id: 13, date_spot_id: 5, city_name: Prefecture.find(13).name + "渋谷区恵比寿4丁目20 ガーデンプレイス")
+# 福岡
+SpotAndAddressCreate("キャナルシティ博多", 1, normal_time("08:00"), normal_time("23:00"), 40, "福岡市博多区住吉1丁目2")
+SpotAndAddressCreate("つなぐダイニング ZINO 天神店", 8, normal_time("20:00"), midnight__time("05:00"), 40, "福岡市中央区大名1-11-22-1")
+SpotAndAddressCreate("大濠公園", 11, "", "", 40, "福岡市中央区大濠公園")
 
-DateSpot.create(id: 6, name: "あか牛丼いわさき", genre_id: 2, image: File.open('./public/images/date_spot_images/aka_ushi_iwasaki.jpg'), opening_time: "2000-01-01 11:00", closing_time: "2000-01-02 00:00")
-Address.create(prefecture_id: 43, date_spot_id: 6, city_name: Prefecture.find(43).name + "阿蘇市乙姫2006-2")
+# 東京
+SpotAndAddressCreate("東京ディズニーランド", 5, normal_time("10:00"), normal_time("19:00"), 12, "墨田区押上1-1-2")
+SpotAndAddressCreate("東京スカイツリー", 11, normal_time("10:00"), normal_time("21:00"), 13, "渋谷区恵比寿4丁目20 ガーデンプレイス")
+SpotAndAddressCreate("恵比寿ガーデンプレイス", 1, normal_time("07:00"), midnight__time("00:00"), 13, "渋谷区恵比寿4丁目20 ガーデンプレイス")
+
+# 熊本
+SpotAndAddressCreate("あか牛丼いわさき", 2, normal_time("11:00"), midnight__time("00:00"), 43, "阿蘇市乙姫2006-2")
