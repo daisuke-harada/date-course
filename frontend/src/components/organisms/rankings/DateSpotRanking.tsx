17910663c0e45178774608893174458b1fbd2448
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-04-09 11:17:40 +0900

    ランキングコンポーネントのレイアウト変更

--- frontend/src/components/organisms/rankings/DateSpotRanking.tsx
+++ frontend/src/components/organisms/rankings/DateSpotRanking.tsx
@@ -1,14 +1,19 @@
 import { memo, VFC } from "react";
 import tw from 'tailwind-styled-components';
 
-const MaindDiv = tw.div`w-11/12 h-96 m-auto mt-6 bg-white border-2 rounded-3xl border-gray-200 justify-center flex py-1`;
+const MaindDiv = tw.div`w-11/12 m-auto mt-6 bg-white border-2 rounded-3xl border-gray-200`;
 
 
 export const DateSpotRanking: VFC = memo((props) => {
 
   return(
     <MaindDiv>
-      <div>ランキング詳細</div>
+      <div className='w-full m-2 text-center font-bold'>全国の人気ランキング</div>
+      <div className='w-full m-auto flex justify-center'>
+        <div className='w-1/3 m-2 h-64 rounded-xl bg-black'></div>
+        <div className='w-1/3 m-2 h-64 rounded-xl bg-black'></div>
+        <div className='w-1/3 m-2 h-64 rounded-xl bg-black'></div>
+      </div>
     </MaindDiv>
   );
 });
