29776f96f8c2f29aec21360a299941d53598d0aa
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-04-21 14:01:17 +0900

    dateSpotのcypressのInput実装

---
+++ e2e/cypress/support/backendAccessMock/dateSpots/apiDateSpotAccess.ts
@@ -0,0 +1,27 @@
+import { addressAndDateSpotTestDatas } from './../../../fixtures/dateSpots/addressAndDateSpotTestDatas';
+import { AddressAndDateSpotJoinData, DateSpotResponseData } from "../../types/dateSpots/response";
+import { spotReviewAndUserTestDatas } from '../../../fixtures/dateSpotReviews/spotReviewTestDatas';
+
+
+export const apiDateSpotCreateAccess = (dateSpotData: DateSpotResponseData) => {
+  cy.intercept('POST', 'api/v1/date_spots', (req) => {
+    req.reply({ status: 'created', dateSpot: dateSpotData });
+  });
+};
+
+export const apiDateSpotShowAccess = (addressAndDateSpotTestData: AddressAndDateSpotJoinData) => {
+  cy.intercept('GET', `api/v1/date_spots/${addressAndDateSpotTestData.dateSpot.id}`, (req) => {
+    const dateSpotReviews = spotReviewAndUserTestDatas.filter((review) => review.dateSpotId === addressAndDateSpotTestData.dateSpot.id);
+    req.reply({
+      addressAndDateSpot: addressAndDateSpotTestData,
+      reviewAverageRate: addressAndDateSpotTestData.averageRate,
+      dateSpotReviews: dateSpotReviews
+    });
+  });
+};
+
+export const apiDateSpotIndexAccess = (addressAndDateSpotTestDatas: AddressAndDateSpotJoinData[]) => {
+  cy.intercept('GET', 'api/v1/date_spots', (req) => {
+    req.reply({ address_and_date_spots:addressAndDateSpotTestDatas });
+  })
+}
