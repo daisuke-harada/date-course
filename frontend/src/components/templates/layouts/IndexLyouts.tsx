f877bc600f9cd03ee51da84e48afb27b0afbdbb6
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-04-26 09:28:05 +0900

    cssのブラッシュアップ

--- frontend/src/components/templates/layouts/IndexLyouts.tsx
+++ frontend/src/components/templates/layouts/IndexLyouts.tsx
@@ -15,10 +15,10 @@ export const IndexLayout: VFC<Props> = memo((props) => {
       <div className='md:block hidden w-3/12 pl-5 pt-5 z-50 fixed'>
         { sideArea }
       </div>
-      <div className='md:hidden w-full z-50 fixed'>
+      <div className='md:hidden w-full z-30 fixed'>
         { topArea }
       </div>
-      <div className='md:w-9/12 md:mt-0 mt-40 w-full p-5'>
+      <div className='md:w-9/12 md:mt-0 mt-52 w-full p-5'>
         { mainArea }
       </div>
     </div>
