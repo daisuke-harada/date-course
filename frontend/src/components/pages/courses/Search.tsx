d772b8d0a5aa4910f3bdf337dc1bf3afd1e99d79
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-06-03 22:46:17 +0900

    コードリファクタリング courses/search.tsxの不要なimportを削除

--- frontend/src/components/pages/courses/Search.tsx
+++ frontend/src/components/pages/courses/Search.tsx
@@ -1,7 +1,6 @@
 import { memo, useEffect, useState, VFC } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { UserNameSearchBar } from 'components/organisms/searchs/UserNameSearchBar';
 import { IndexLayout } from 'components/templates/layouts/IndexLyouts';
 import { CourseResponseData } from 'types/courses/response';
 import { Courses } from 'components/templates/courses/Courses';
@@ -9,8 +8,6 @@ import { CourseSortSearchBar } from 'components/organisms/searchs/CourseSortSear
 import { MultiBar } from 'components/organisms/searchs/MultiBar';
 import { defaultUserResponseData } from 'datas/defaultUserData';
 import { defaultAddfressAndDateSpotJoinData } from 'datas/defaultAddressAndDateSpotJoinData';
-import { DateSpotSortSearchBar } from 'components/organisms/searchs/DateSpotSortSearchBar';
-import { DateSpotNameSearchBar } from 'components/organisms/searchs/DateSpotNameSearchBar';
 
 export const Search: VFC = memo(() => {
   const [courses, setCourses] = useState<CourseResponseData[]>([
