40b53808f5c37280b2fccb1c58059ff3435fb759
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2023-09-27 23:04:07 +0900

    fix: メソッドの呼び出し元をよりわかりやすいところに変更

--- backend/app/controllers/api/v1/genres_controller.rb
+++ backend/app/controllers/api/v1/genres_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GenresController < ApplicationController
   def show
     date_spots = DateSpot.where(genre_id: params[:id])
     @address_and_date_spots = date_spots.map do |date_spot|
-      date_spot.address.address_and_date_spot_and_genre_name
+      date_spot.address.combined_data_with_address_and_genre
     end
     render json: {address_and_date_spots: @address_and_date_spots}
   end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,27 +1,2 @@
 class ApplicationController < ActionController::API
-  # 評価の平均値を計算する
-  def average_rate_calculation(reviews)
-    review_rate_total = 0
-    reviews.each { |review| review_rate_total += review.rate }
-    (review_rate_total == 0) ? 0 : review_rate_total / reviews.length
-  end
-
-  # デートコース関連
-  # コース内のデートスポットの情報を配列にして返す
-  def during_address_and_date_spots(course)
-    course.during_spots.map do |during_spot|
-      Address.find_by(date_spot_id: during_spot.date_spot_id).address_and_date_spot_and_genre_name
-    end
-  end
-
-  # コース内のルート範囲の県名を返す
-  def course_prefecture_names_no_duplicate(course)
-    prefecture_name = course.during_spots.map do |during_spot|
-      # during_spotから県名を取り出す
-      Prefecture.find(Address.find_by(date_spot_id: during_spot.date_spot_id).prefecture_id).name
-    end
-
-    # 県名の重複をなくして返す
-    prefecture_name.uniq
-  end
 end
