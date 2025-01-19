db1a6071b64f51873f47d912f135f4da6cffc4e4
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2025-01-19 15:06:15 +0900

    fix: cors.rb

--- backend_rails/config/initializers/cors.rb
+++ backend_rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONTEND_DOMAIN"]
+    origins [ENV["FRONTEND_DOMAIN"], "http://localhost:8080"]
 
     resource "*",
       headers: :any,
