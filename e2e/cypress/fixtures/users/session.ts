d082a8ff2b1bc59166f8cde20560bea9554fd21f
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-24 22:58:03 +0900

    fix

--- e2e/cypress/fixtures/users/session.ts
+++
@@ -1,9 +0,0 @@
-import { testUser } from "./response"
-
-export const testUserInput = {
-  name: testUser.name,
-  email: testUser.email,
-  password: testUser.name,
-  passwordConfirmation: testUser.name,
-  gender: testUser.gender,
-}
